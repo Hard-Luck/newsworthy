8ac8ffe1e20220d6440184cb4d4f5b4925bce2c4
Mark Tyree <tyreemarka@gmail.com>
2023-07-04 14:46:07 +0100

    added prettier config and db util to add password to users

--- src/db/data/development-data/users.ts
+++ src/db/data/development-data/users.ts
@@ -1,46 +1,51 @@
 const users = [
   {
-    username: "tickle122",
-    password: "tickle122",
-    name: "Tom Tickle",
+    username: 'tickle122',
+    password:
+      '$2a$10$3In3Q.MvEYGn7m8dka4N.ud.oZ84Tbi.rDoHNO1p2tr88A2phnnqi',
+    name: 'Tom Tickle',
     avatar_url:
-      "https://vignette.wikia.nocookie.net/mrmen/images/d/d6/Mr-Tickle-9a.png/revision/latest?cb=20180127221953",
+      'https://vignette.wikia.nocookie.net/mrmen/images/d/d6/Mr-Tickle-9a.png/revision/latest?cb=20180127221953',
   },
   {
-    username: "grumpy19",
-    password: "grumpy19",
-    name: "Paul Grump",
+    username: 'grumpy19',
+    password:
+      '$2a$10$nMXoFcqt8H713SF8VmKR8ORfxwwBDHdTl0fHsEip.Z2omg4qCBqzO',
+    name: 'Paul Grump',
     avatar_url:
-      "https://vignette.wikia.nocookie.net/mrmen/images/7/78/Mr-Grumpy-3A.PNG/revision/latest?cb=20170707233013",
+      'https://vignette.wikia.nocookie.net/mrmen/images/7/78/Mr-Grumpy-3A.PNG/revision/latest?cb=20170707233013',
   },
   {
-    username: "happyamy2016",
-    password: "happyamy2016",
-    name: "Amy Happy",
+    username: 'happyamy2016',
+    password:
+      '$2a$10$je.5Zmx2k6qcBpmsJglvEOZ2WXcEkdOCmOqKZVuas.DjNTKU6V3JO',
+    name: 'Amy Happy',
     avatar_url:
-      "https://vignette1.wikia.nocookie.net/mrmen/images/7/7f/Mr_Happy.jpg/revision/latest?cb=20140102171729",
+      'https://vignette1.wikia.nocookie.net/mrmen/images/7/7f/Mr_Happy.jpg/revision/latest?cb=20140102171729',
   },
   {
-    username: "cooljmessy",
-    password: "cooljmessy",
-    name: "Peter Messy",
+    username: 'cooljmessy',
+    password:
+      '$2a$10$K4HTq5SQIKTePcKwnClCoOc5rw60iA4bu8PuwduGSomdsPbp74w7y',
+    name: 'Peter Messy',
     avatar_url:
-      "https://vignette.wikia.nocookie.net/mrmen/images/1/1a/MR_MESSY_4A.jpg/revision/latest/scale-to-width-down/250?cb=20170730171002",
+      'https://vignette.wikia.nocookie.net/mrmen/images/1/1a/MR_MESSY_4A.jpg/revision/latest/scale-to-width-down/250?cb=20170730171002',
   },
   {
-    username: "weegembump",
-    password: "weegembump",
-    name: "Gemma Bump",
+    username: 'weegembump',
+    password:
+      '$2a$10$O3AOuYByudICKd0X2B4X/OwjDCL./i.Ocq6vRazZCXyBSXM15oHL6',
+    name: 'Gemma Bump',
     avatar_url:
-      "https://vignette.wikia.nocookie.net/mrmen/images/7/7e/MrMen-Bump.png/revision/latest?cb=20180123225553",
+      'https://vignette.wikia.nocookie.net/mrmen/images/7/7e/MrMen-Bump.png/revision/latest?cb=20180123225553',
   },
   {
-    username: "jessjelly",
-    password: "jessjelly",
-    name: "Jess Jelly",
+    username: 'jessjelly',
+    password:
+      '$2a$10$HB6.UTenHUdPf615RMTN..heEegxv0uZeqV4vMhd.13o812/JyEpi',
+    name: 'Jess Jelly',
     avatar_url:
-      "https://vignette.wikia.nocookie.net/mrmen/images/4/4f/MR_JELLY_4A.jpg/revision/latest?cb=20180104121141",
+      'https://vignette.wikia.nocookie.net/mrmen/images/4/4f/MR_JELLY_4A.jpg/revision/latest?cb=20180104121141',
   },
-];
-
-export default users;
+]
+export default users
